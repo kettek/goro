c7173a228b29ac5035a186e19f97bd67c0312a47
kts of kettek (nyaa) <kettek1@kettek.net>
2019-06-26 13:46:41 -0700

    Make ebiten by enable choice only

--- backend_ebiten.go
+++ backend_ebiten.go
@@ -16,7 +16,7 @@ You should have received a copy of the GNU Lesser General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-// +build !disableEbiten
+// +build enableEbiten,!disableEbiten
 
 package goro
 
