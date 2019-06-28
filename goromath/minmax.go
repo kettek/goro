158d1ef44ccfcf48b6328ae968fbcb990a957c7d
kts of kettek (nyaa) <kettek1@kettek.net>
2019-06-28 04:22:46 -0700

    Add goromath package and MinInt&MaxInt functions

---
+++ goromath/minmax.go
@@ -0,0 +1,35 @@
+/*
+This file is a part of goRo, a library for writing roguelikes.
+Copyright (C) 2019 Ketchetwahmeegwun T. Southall
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+package goromath
+
+// MinInt returns the smaller of two integers.
+func MinInt(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+// MaxInt returns the larger of two integers.
+func MaxInt(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
