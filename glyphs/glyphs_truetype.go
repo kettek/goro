10e86bfa41570ef0d95350ac201d543f7cd8a8ca
kts of kettek (nyaa) <kettek1@kettek.net>
2019-07-13 18:57:17 -0700

    Adjust method for getting glyph width

--- glyphs/glyphs_truetype.go
+++ glyphs/glyphs_truetype.go
@@ -80,12 +80,13 @@ func (f *Truetype) rebuild() {
 	metrics := f.Normal.Metrics()
 	f.height = (metrics.Ascent + metrics.Descent).Round()
 
-	bounds, _, ok := f.Normal.GlyphBounds('M')
+	f.width = int(f.truetype.FUnitsPerEm())
+	/*bounds, _, ok := f.Normal.GlyphBounds('M')
 	if !ok {
 		f.width = f.height / 2 // This is not good.
 	} else {
 		f.width = bounds.Max.X.Round()
-	}
+	}*/
 
 	f.ascent = metrics.Ascent.Round()
 }
