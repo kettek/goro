866d12132ac6ced98f706215c0dbca35fc86aee7
kts of kettek <kts@kettek.net>
2019-07-16 23:53:24 -0700

    Remove first step from steps return

--- pathing/path_astar.go
+++ pathing/path_astar.go
@@ -172,7 +172,12 @@ func (p *PathAStar) tracePath(tX, tY int) (steps []Step) {
     y = tempY
   }
 
-  steps = append([]Step{Step{x: x, y: y}}, steps[1:]...)
+  steps = append([]Step{Step{x: x, y: y}}, steps...)
+
+  // Remove first step.
+  if len(steps) > 0 {
+    steps = steps[1:]
+  }
   return
 }
 
