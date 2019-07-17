82ecfa7879f2a9ab4928537e194b4b8e4d5aefa5
kts of kettek <kts@kettek.net>
2019-07-17 00:49:26 -0700

    Convert to Euclideann distance for heuristics

--- pathing/path_astar.go
+++ pathing/path_astar.go
@@ -179,10 +179,14 @@ func (p *PathAStar) Compute(oX, oY int, tX, tY int) (steps []Step) {
 }
 
 func (p *PathAStar) calculateH(x, y int, tX, tY int) float64 {
-  return math.Max(math.Abs(float64(x - tX)), math.Abs(float64(y - tY)))
-  /*a := math.Pow(float64(y-tY), 2)
+  // Manhattan Distance
+  //return math.Abs(float64(x - tX)) + math.Abs(float64(y - tY))
+  // Diagonal Distance
+  //return math.Max(math.Abs(float64(x - tX)), math.Abs(float64(y - tY)))
+  // Euclidean Distance
+  a := math.Pow(float64(y-tY), 2)
   b := math.Pow(float64(x-tX), 2)
-  return math.Sqrt(a + b)*/
+  return math.Sqrt(a + b)
 }
 
 func (p *PathAStar) tracePath(tX, tY int) (steps []Step) {
